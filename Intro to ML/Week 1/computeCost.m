8bf617950ae72214f2c81dd3333322eff30c4634
priyanka24798 <kotupriyanka@msitprogram.net>
2020-09-16 01:09:07 +0530

    Week 1

---
+++ Intro to ML/Week 1/computeCost.m
@@ -0,0 +1,13 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+i = 1:m;
+J = (1/(2*m)) * sum( ((theta(1) + theta(2) .* X(i,2)) - y(i)) .^ 2);
+
+
+
+end
